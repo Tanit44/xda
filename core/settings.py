a83c3a2a0b4f7dbcf761c29e0d302f38d6f93c7a
Tanit44 <tanit.t44@gmail.com>
2022-11-26 11:16:28 +0700

    edit

--- core/settings.py
+++ core/settings.py
@@ -79,12 +79,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lifjginb',
+        'USER': 'lifjginb',
+        'PASSWORD': 'xIXxhudrUVdt1l6p95Od9Plhv7mVVGCJ',
+        'HOST': 'lucky.db.elephantsql.com',
+        'PORT': '5432'
+     }
+   }
 
 
 # Password validation
